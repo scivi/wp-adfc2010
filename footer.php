61e035c44b2b3c8c4fadf9038973d87ef41d19fd
Christof Glaser <gcg@gl.aser.de>
2010-05-18 16:19:24 +0200

    Footer: style widget ul even better

--- footer.php
+++ footer.php
@@ -4,10 +4,10 @@
 			<div class="clearer"></div>
 		</div>
 		<div class="footer">
-			<p class="noMargin noPrint">
-				&copy; ADFC Sachsen-Anhalt <?php echo date('Y'); ?> &ndash; 
-				<div style="display:inline-block;"><ul><?php dynamic_sidebar( 'footer-widget-area' ); ?></ul></div>
+			<p class="noMargin noPrint" style="float:left;">
+				&copy; ADFC Sachsen-Anhalt <?php echo date('Y'); ?> &ndash;
 			</p>
+			<ul style="float:left;"><?php dynamic_sidebar( 'footer-widget-area' ); ?></ul>
 		</div>
 		<?php wp_footer(); ?>
 	</body>

7d406a1ee9cd38c3087e5541c6d0df560d94927b
Christof Glaser <gcg@gl.aser.de>
2010-05-19 00:08:01 +0200

    Index: show subpages, if any, improved more

--- index.php
+++ index.php
@@ -44,9 +44,9 @@ get_template_part( 'loop', 'index' ); // all stuff
 					</div>
 					<div class="rightCol">
 <?php /* Display navigation to next/previous pages when applicable */
-	$children_for = array_shift(remember('the_id'));
-	if ($children_for == -1) { $children = false; echo 'foo'; }
-	else { print_r($children_for); $children = wp_list_pages("title_li=&child_of=${children_for}&echo=0"); }
+	$children_for = remember('the_id');
+	if (is_numeric($children_for) { $children = wp_list_pages("title_li=&child_of=${children_for}&echo=0"); }
+	else { print_r($children_for); $children = false; echo 'foo'; }
 	$has_pages = $wp_query->max_num_pages > 1;
 ?>
 <?php if ($has_pages || $children) : ?>
--- loop.php
+++ loop.php
@@ -136,7 +136,7 @@
 		<?php // for the right sidebar; comes after the loop
 			remember('categories', get_the_category());
 			remember('tags', get_the_tags());
-			if (!is_archive() && !is_search()) remember('the_id', get_the_ID()); else remember('the_id', -1);
+			if (!is_archive() && !is_search()) remember('the_id', get_the_ID()); else remember('the_id', 'none');
 		?>
 			<div class="entry-utility hidden">
 				<span class="comments-link">

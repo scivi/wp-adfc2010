ec906e37d64ee4e2fe8b14d6c3ff9859ee897b58
Christof Glaser <gcg@gl.aser.de>
2010-05-18 16:22:09 +0200

    Footer: style even better

--- footer.php
+++ footer.php
@@ -4,10 +4,10 @@
 			<div class="clearer"></div>
 		</div>
 		<div class="footer">
-			<p class="noMargin noPrint" style="float:left;">
+			<p class="noMargin noPrint leftish">
 				&copy; ADFC Sachsen-Anhalt <?php echo date('Y'); ?> &ndash;
 			</p>
-			<ul style="float:left;"><li><a href="/">Home</a></li><?php dynamic_sidebar( 'footer-widget-area' ); ?></ul>
+			<ul class="leftish"><li><a href="/">Home</a></li><?php dynamic_sidebar( 'footer-widget-area' ); ?></ul>
 		</div>
 		<?php wp_footer(); ?>
 	</body>

94bbabf9a2a16814594ef29472cc730e4c1b374a
Christof Glaser <gcg@gl.aser.de>
2010-05-21 03:47:11 +0200

    Index: hide redundant category in Verwandte Themen -- fix done for pages

--- index.php
+++ index.php
@@ -102,7 +102,7 @@ if ($cats = uniq_objects(remember('categories'), 'cat_ID')) : ?>
 								foreach($cats as $cat) {
 									$url = get_category_link($cat->cat_ID);
 									$cur = $_SERVER['REQUEST_URI'];
-									echo substr($cur, 0, strpos($cur, '/page/'));
+									if (substr($url, 29) == $cur) continue;
 									if (substr($url, 29) == substr($cur, 0, strpos($cur, '/page/'))) continue;
 									echo "<a class=\"blockLink singleArrow\" href=\"$url\">$cat->cat_name</a>";
 								}; ?>
